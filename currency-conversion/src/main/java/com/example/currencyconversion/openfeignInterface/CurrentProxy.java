d78753a8ec01caeb080debe718ff3b2598258dcb
mahboobdorali <mahboobdorali@gmail.com>
2023-05-29 12:36:19 +0430

    add @FeignClient in this interface

--- currency-conversion/src/main/java/com/example/currencyconversion/openfeignInterface/CurrentProxy.java
+++ currency-conversion/src/main/java/com/example/currencyconversion/openfeignInterface/CurrentProxy.java
@@ -1,4 +1,9 @@
 package com.example.currencyconversion.openfeignInterface;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "currency-exchange",url = "localhost:8000")
+//توی ورودی اسم اون ماژولی رو قرار میدیم که میخوایم بهش request بزنم و توی .propertie بهش اسم دادم
 public interface CurrentProxy {
+
 }
