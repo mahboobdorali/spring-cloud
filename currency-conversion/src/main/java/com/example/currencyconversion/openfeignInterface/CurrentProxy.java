0a03c55c61d14f5b8f9706bc8bc9f3214a6ef1df
mahboobdorali <mahboobdorali@gmail.com>
2023-05-30 08:48:31 +0330

    fixed the port number

--- currency-conversion/src/main/java/com/example/currencyconversion/openfeignInterface/CurrentProxy.java
+++ currency-conversion/src/main/java/com/example/currencyconversion/openfeignInterface/CurrentProxy.java
@@ -5,10 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange", url = "localhost:8001")
 //توی ورودی اسم اون ماژولی رو قرار میدیم که میخوایم بهش request بزنم و توی .propertie بهش اسم دادم
+//پورتی رومیدیم که currency-exchange روش داره میاد بالا
+
 public interface CurrentProxy {
-    @GetMapping("http://localhost:8001/currency-exchange/firstname/{firstname}/lastname/{lastname}")
+    @GetMapping("currency-exchange/firstname/{firstname}/lastname/{lastname}")
     CurrencyExchange exchange(@PathVariable String firstname,
                               @PathVariable String lastname);
 }
